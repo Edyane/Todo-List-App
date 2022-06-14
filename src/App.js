f8b7d6fe2fd382c917c7cb79817760760f655180
Edyane <edyanealves@gmail.com>
2022-06-14 20:17:53 -0300

    Adicionando lista.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className='todo-app'>
-      <TodoForm />
+      <TodoList />
       
     </div>
   );
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-
 function TodoForm(props) {
 const [input, setInput] = useState('');
 
@@ -10,10 +9,10 @@ const handleChange = e => {
 const handleSubmit = e => {
     e.preventDefault();
 
-    //props.onSubmit({
-    //    id: Math.floor(Math.random() * 10000),
-    //    text: input
-    //})
+    props.onSubmit({
+    id: Math.floor(Math.random() * 10000),
+    text: input
+    })
 
     setInput('');
 }
