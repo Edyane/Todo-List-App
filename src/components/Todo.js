9e7b856b1bcff2c6348afdcff7ef8232b6ffd94c
Edyane <edyanealves@gmail.com>
2022-06-15 21:44:43 -0300

    Adicionando Hooks

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,11 +3,23 @@ import TodoForm from './TodoForm';
 import { RiCloseCircleLine } from 'react-icons/ri'
 import { TiEdit } from 'react-icons/ti'
 
-function Todo({todos, completeTodo, removeTodo}) {
+function Todo({todos, completeTodo, removeTodo, updateTodo}) {
     const [edit, setEdit] =useState ({ 
         id: null,
         value: ''
-    })
+    });
+
+    const submitUpdate = value => {
+        updateTodo(edit.id, value)
+        setEdit ({
+            id: null, 
+            value: ''
+        })
+    };
+
+    if (edit.id) {
+        return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+    }
 
     return todos.map((todo, index) => (
         <div className={todo.isComplete ? 'todo-row complete' :
